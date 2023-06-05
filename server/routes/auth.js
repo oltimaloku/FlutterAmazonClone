441915a6b5852411b7d69a5ecb1a91ca3b0282e5
Olti Maloku <oltimaloku555@gmail.com>
2023-06-05 13:59:41 -0700

    set up back end for signin api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,9 @@ const express = require("express")
 const User = require("../models/user");
 const authRouter = express.Router();
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 authRouter.post('/api/signup', async (req, res) => {
-console.log(req.body);
     try {
         // gives us access to name email and passoword
     const {name, email, password} = req.body;
@@ -36,4 +36,27 @@ console.log(req.body);
     
 });
 
+
+// Sign In Route 
+// 
+
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({msg: 'User with this email does not exist!'});
+        }
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({msg: 'Incorrect Password'});
+        }
+
+        const token = jwt.sign({id: user._id}, "PasswordKey");
+        res.json({token, ...user._doc})
+
+    } catch (e){
+        res.status(500).json({error: e.message})
+    }
+});
 module.exports = authRouter;
